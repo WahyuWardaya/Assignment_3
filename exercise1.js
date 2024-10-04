a49359039bc716d6112a43e611ca00363b7f3c72
Wahyu Wardaya <wardaya56@gmail.com>
2024-10-04 13:07:42 +0800

    update console.log exercise1

--- exercise1.js
+++ exercise1.js
@@ -16,4 +16,5 @@ function isArithmeticProgression (numbers) {
 console.log(isArithmeticProgression([1,2,3,4,5,6]));
 console.log(isArithmeticProgression([2,4,6,12,24]));
 console.log(isArithmeticProgression([2,4,6,8]));
+console.log(isArithmeticProgression([2,6,18,54]));
 console.log(isArithmeticProgression([1,2,3,4,7,9]));
